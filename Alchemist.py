5b19d92fcb6763f405cbdddf51d085c9e79b17b5
Arkhan <22105395+Arkhanixe@users.noreply.github.com>
2018-09-08 17:35:39 -0600

    Create Alchemist.py

---
+++ Alchemist.py
@@ -0,0 +1,72 @@
+import discord
+import sqlite3
+from discord.ext import commands
+import random
+
+# Set up logging
+
+extensions = {
+'cogs.cmds',
+"cogs.commands",
+"cogs.admin"
+}   # add more here later
+
+bot = commands.Bot(command_prefix='a!')
+
+# If we fail to load an extension, we just leave it out.
+for extension in extensions:
+	try:
+		bot.load_extension(extension)
+	except Exception as e:
+		print(e)
+
+@bot.event
+async def on_command_completion(ctx):
+    await ctx.message.delete()
+
+@bot.listen()
+async def on_member_join(member):
+		role = discord.utils.get("Member")
+		await member.add_roles(role)
+
+@bot.event
+async def on_ready():
+	print(f"Logged in as {bot.user.name}")
+	print(f"ID : {bot.user.id}")
+	print(f"Preparing Game")
+	game = discord.Game(" Nothing | Guild Count: 3")
+	await bot.change_presence(status=discord.Status.online,activity=game)
+	print(f"Playing {game}")
+
+conn = sqlite3.connect("database.db")
+c = conn.cursor()
+
+c.execute("CREATE TABLE IF NOT EXISTS bank(User_ID BIGINT NOT NULL, Balance float)")
+conn.commit()
+
+
+@bot.command()
+async def register(ctx):
+	c.execute("INSERT INTO bank VALUES(?, ?)",(ctx.author.id, 0))
+	conn.commit()
+
+#@commands.cooldown(1, 30, commands.BucketType.user)
+@bot.command()
+async def work(ctx):
+	Balance = c.execute("SELECT Balance FROM bank WHERE User_ID = ?",(ctx.author.id,)).fetchall()
+	y = Balance[0]
+	z = y[0]
+	x = 1
+	Boxed = (x *)+ z 
+	c.execute("UPDATE bank SET balance=? WHERE user_id=?",(Boxed,ctx.author.id))
+	conn.commit()
+
+@bot.command()
+async def balance(ctx):
+	Balance = c.execute("SELECT Balance FROM bank WHERE User_ID = ?",(ctx.author.id,)).fetchall()
+	await ctx.send(Balance)
+
+@bot.command()
+async def top(ctx):
+	Alpha = c.execute("SELECT * FROM bank ORDER BY balance DESC").fetchall()
+	await ctx.send(Alpha[:10])
