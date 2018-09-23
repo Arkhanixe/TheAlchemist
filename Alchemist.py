e790b5843c16b7d4666cdcd271d977e3c0db1751
Arkhan <22105395+Arkhanixe@users.noreply.github.com>
2018-09-22 22:01:51 -0600

    Add files via upload

--- Alchemist.py
+++ Alchemist.py
@@ -96,7 +96,7 @@ async def save(ctx,*args):
 async def suggest(ctx, *, msg):
     x = await ctx.bot.get_channel(492274127709929482)
     embed = discord.Embed(title="Suggestion",description=f"{ctx.author.name} | ID : {ctx.author.id} has sent Suggestion {msg}")
-    await x.send(embed=embed)
+    x.send(embed=embed)
     em = discord.Embed(title="Suggestion sent",description=f"Message | \n {msg} |\n was sent")
     await ctx.send(embed=em)
 
