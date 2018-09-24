6a9ffb62ddad268347f0d07a85b2e8326a463147
Arkhan <22105395+Arkhanixe@users.noreply.github.com>
2018-09-24 16:05:20 -0600

    Update Alchemist.py

--- Alchemist.py
+++ Alchemist.py
@@ -180,7 +180,6 @@ async def on_command_completion(ctx):
 @commands.is_owner()
 @bot.command()
 async def restart(ctx):
-	os.system("git pull")
 	with open("Token.txt") as fp:
 		token = fp.read().strip()
 
@@ -192,7 +191,7 @@ async def on_ready():
 	print(f"Logged in as {bot.user.name}")
 	print(f"ID : {bot.user.id}")
 	print(f"Preparing Game")
-	game = discord.Game(f" Nothing | Guild Count: {len(bot.guild)}")
+	game = discord.Game(f" Nothing | Guild Count: {len(bot.guilds)}")
 	await bot.change_presence(status=discord.Status.online,activity=game)
 	print(f"Playing {game}")
 
