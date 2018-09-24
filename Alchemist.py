6405b8c4e2f124f9bdc5c6ee3c8331316a80903f
Arkhan <22105395+Arkhanixe@users.noreply.github.com>
2018-09-24 12:07:49 -0600

    Add files via upload

--- Alchemist.py
+++ Alchemist.py
@@ -181,7 +181,7 @@ async def on_ready():
 	print(f"Logged in as {bot.user.name}")
 	print(f"ID : {bot.user.id}")
 	print(f"Preparing Game")
-	game = discord.Game(" Nothing | Guild Count: 3")
+	game = discord.Game(" Nothing | Guild Count: {len(bot.guilds)}")
 	await bot.change_presence(status=discord.Status.online,activity=game)
 	print(f"Playing {game}")
 
