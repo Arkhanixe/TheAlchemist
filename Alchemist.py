b183b403dde08c51da9126e0fc50181d1425f2af
Arkhan <22105395+Arkhanixe@users.noreply.github.com>
2018-10-18 20:46:46 -0600

    Update Alchemist.py

--- Alchemist.py
+++ Alchemist.py
@@ -97,7 +97,7 @@ async def help(ctx):
 @bot.listen()
 async def on_member_join(member):
     embed = discord.Embed(title=f"{member.display_name} Joined",color=0x00FF00)	
-    embed.add_field(name=f"Creation Date",value=f"{member.created_at}",inline=True)
+    embed.add_field(name=f"Creation Date",value=f"{member.created_at}")
     embed.set_footer(url=ctx.author.avatar_url)
     try:
         channel = discord.utils.get(member.guild.channels, name="general")
