7ea5b01c3f6e71f097a6cf71bd34beabc2fd4db5
Arkhan <22105395+Arkhanixe@users.noreply.github.com>
2018-10-19 10:37:34 -0600

    Update Alchemist.py

--- Alchemist.py
+++ Alchemist.py
@@ -121,6 +121,12 @@ async def on_member_remove(member):
     except:
         pass
 
+@bot.listen()
+async def on_guild_join(guild):
+	embed = discord.Embed(Title="Joined Guild",description=f"Thank you for inviting me! My name is Alchemex. My prefix is <a!>. My help server is a!server. Have a nice day! \nAlchemex Creators")
+	
+	
+
 with open("Token.txt") as fp:
     token = fp.read().strip()
 
