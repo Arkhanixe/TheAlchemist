12c3e2af6aecef5bee77bcc1b1e6602c31865601
Justin <22105395+ProjectXTeam@users.noreply.github.com>
2018-10-24 21:33:15 -0600

    Update Alchemist.py

--- Alchemist.py
+++ Alchemist.py
@@ -109,8 +109,8 @@ async def help(ctx):
 @bot.listen()
 async def on_member_join(member):
     embed = discord.Embed(title=f"{member.name}#{member.discriminator}",color=0x00FF00)	
-    embed.add_field(name=f"Creation Date",value=f"{member.created_at.strftime('%B %d, %Y')}",inline=True)
-    embed.add_field(name=f"Join Date", value=f"{member.joined_at.strftime('%B %d, %Y')}",inline=True)
+    embed.add_field(name=f"Creation Date",value=f"{member.created_at.strftime(%B %d, %Y)}",inline=True)
+    embed.add_field(name=f"Join Date", value=f"{member.joined_at.strftime(%B %d, %Y)}",inline=True)
     embed.set_author(name="Member Joined",icon_url=member.avatar_url)
     try:
         channel = discord.utils.get(member.guild.channels, name="general")
@@ -137,8 +137,8 @@ async def on_member_join(member):
 @bot.listen()
 async def on_member_remove(member):
     embed = discord.Embed(title=f"{member.name}#{member.discriminator}",color=0xFF0000)	
-    embed.add_field(name=f"Creation Date",value=f"{member.created_at.strftime('%B %d, %Y %I:%M %p')}",inline=True)
-    embed.add_field(name=f"Join Date", value=f"{member.joined_at.strftime('%B %d, %Y %I:%M %p')}",inline=True)
+    embed.add_field(name=f"Creation Date",value=f"{member.created_at.strftime(%B %d, %Y %I:%M %p)}",inline=True)
+    embed.add_field(name=f"Join Date", value=f"{member.joined_at.strftime(%B %d, %Y %I:%M %p)}",inline=True)
     embed.set_author(name="Member left",icon_url=member.avatar_url)
     try:
         channel = discord.utils.get(member.guild.channels, name="general")
